3afa33a6f3e2eca31296830e19c04197dfaa02f0
Bhavya Bibi <bhavyabibi007@gmail.com>
2021-02-22 15:44:07 +0400

    javascript be added

---
+++ Painting App Js/script.js
@@ -0,0 +1,12 @@
+const canvas = document.getElementById("canvas")
+const ctx = canvas.getContext("2d");
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, 500, 500);
+ctx.fillStyle = "black";
+let brushSize = "black";
+document.getElementById("color").addEventListener("change", function () {
+    brushColor = this.value;
+})
+document.getElementById("thicknes").addEventListener("change", function () {
+    brushSize = this.value;
+})
